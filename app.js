b7cef2c4dbe9b3f1c32e8a32f06eb92c2a833eab
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-27 22:46:55 -0500

    started code, added data, started index.html

---
+++ app.js
@@ -0,0 +1,17 @@
+var tempData = temp_data
+var stdData = std_data
+console.log("temp data", tempData)
+console.log('std data', stdData)
+
+function lineChart(filteredList) {
+
+}
+
+// TODO 
+    // 1. line chart showing one country, all years, 12 lines for month of diff colors
+    // 2. line chart showing one country, one year, months on x
+    // 3. line chart showing one country, one month, all years
+    // **grey out year or month if they pick the other**
+    // chart showing hemisphere and/or continent
+    // 4.get current demo of country from census data
+    // 5. for month, avg out all the years
